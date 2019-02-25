310f604109c44f7d4995bf0d41852094668ed66c
peefer66 <peefer1966@gmail.com>
2019-02-25 19:00:21 +0000

    created category table

--- compounds/models.py
+++ compounds/models.py
@@ -4,13 +4,28 @@ from application import db
 
 class Compound(db.Model):
     id = db.Column(db.Integer,primary_key=True)
+    category_id = db.Column(db.Integer,db.ForeignKey('category.id'))
     cpd_no = db.Column(db.String(25),unique=True)
     comments = db.Column(db.String(255))
     date_issued = db.Column(db.DateTime)
+
+    category = db.relationship('Category',backref=db.backref('compounds',lazy='dynamic'))
+
+
 
     def __init__(self,cpd_no,comments,date_issued):
         self.cpd_no = cpd_no
         self.comments = comments
         self.date_issued = date_issued
-    def __retr__(self):
+    def __repr__(self):
         return f'Compound Number: {self.cpd_no}'
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(50),unique=True)
+
+    def __init__(self,category):
+        self.category = category
+    
+    def __repr__(self):
+        return f'Category: {self.category}'
