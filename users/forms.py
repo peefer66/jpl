5237377abf7ed065917d5a8dece335dcec4a0ec9
peefer66 <peefer1966@gmail.com>
2019-02-28 06:48:20 +0000

    Registration.html formhelpers and nav

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
+#USERS/FORM.PY
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, ValidationError
-from wtforms.valdators import DataRequired, Email, EqualTo, Length, InputRequired, Required
+from wtforms.validators import DataRequired, Email, EqualTo, Length, InputRequired, Required
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 
@@ -15,13 +17,17 @@ class RegisterForm(FlaskForm):
     email = StringField('Email',
             validators=[InputRequired('Please enter your email address'),
                         Email()])
+
     password = PasswordField('Password',
                 validators=[InputRequired('Please enter a password'),
                             Length(min=6,max=25)])
+
     confirm_password = PasswordField('Confirm Password',
                         validators=[InputRequired('Confirm password'),
                         EqualTo('password', message = 'Passwords must match')])
-    submit = SubmitField('Register')
+    
+    submit = SubmitField('Register')    
+
 
     def check_username(self,field):
         if User.query.filter_by(username=field.data).first():
--- users/views.py
+++ users/views.py
@@ -1,15 +1,19 @@
 # USERS/VIEWS.PY
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, render_template
+
 
 from users.models import User
 from application import login_manager
+from users.forms import RegisterForm
 
 user_bp = Blueprint('user_bp',__name__)
 
 ###### REGSTER ######
-@user_bp.route('/register')
+@user_bp.route('/register', methods=['GET','POST'])
 def register():
-    return 'Registered'
+    form = RegisterForm()
+
+    return render_template('users/register.html', form=form)
 
 ###### LOGIN #######
 @user_bp.route('/login')
