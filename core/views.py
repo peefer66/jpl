08a21949dee20fb625fddb077bb82cd355eec922
peefer66 <peefer66@hotmail.com>
2019-03-14 08:12:34 +0000

    started index.html

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 #CORE/VIEWS.PY
 
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, render_template, redirect
 from flask_login import login_required
 
 core_bp = Blueprint('core_bp',__name__)
@@ -8,4 +8,4 @@ core_bp = Blueprint('core_bp',__name__)
 @core_bp.route('/')
 @login_required
 def index():
-    return 'Hello There'
+    return render_template('core/index.html')
